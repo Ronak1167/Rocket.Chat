a1e00c5539f20c62006b05f4a98d28bb5b71e616
Pierre Lehnen <pierre.lehnen@rocket.chat>
2026-06-30 13:57:36 -0300

    Disable transfer button when feature is disabled

--- packages/ui-voip/src/views/MediaCallRoomSection/MediaCallRoomSection.tsx
+++ packages/ui-voip/src/views/MediaCallRoomSection/MediaCallRoomSection.tsx
@@ -74,6 +74,7 @@ const MediaCallRoomSection = ({ showChat, onToggleChat, user, containerHeight }:
 
 	const screenShareAvailable = features.includes('screen-share');
 	const holdAvailable = features.includes('hold');
+	const transferAvailable = features.includes('transfer');
 
 	if (!peerInfo || !('userId' in peerInfo) || !peerInfo.userId) {
 		return null;
@@ -133,7 +134,9 @@ const MediaCallRoomSection = ({ showChat, onToggleChat, user, containerHeight }:
 					/>
 				)}
 
-				<ActionButton disabled={connecting || reconnecting} label={t('Forward')} icon='arrow-forward' onClick={onForward} />
+				{transferAvailable && (
+					<ActionButton disabled={connecting || reconnecting} label={t('Forward')} icon='arrow-forward' onClick={onForward} />
+				)}
 				<ActionButton label={t('Voice_call__user__hangup', { user: peerInfo.displayName })} icon='phone-off' danger onClick={onEndCall} />
 			</ActionStrip>
 		</Box>
